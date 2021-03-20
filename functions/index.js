74195601cccdb78fed286039bebbe6f05669e6bf
Val Delchev <btwonu@gmail.com>
2021-03-20 22:29:21 +0200

    Add initial movie routes

--- functions/index.js
+++ functions/index.js
@@ -29,4 +29,23 @@ app.get('/', (req, res) => {
   //   });
 });
 
+// Movie routes
+app.get('/movies/popular', async (req, res) => {
+  movieService.getPopular().then((movies) => {
+    res.json(movies);
+  });
+});
+
+app.get('/movies/top-rated', (req, res) => {
+  movieService.getTopRated().then((movies) => {
+    res.json(movies);
+  });
+});
+
+app.get('/movies/upcoming', (req, res) => {
+  movieService.getUpcoming().then((movies) => {
+    res.json(movies);
+  });
+});
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
