92b8e6343dee502cc914601a9cf353a9eaae5352
Val Delchev <btwonu@gmail.com>
2021-03-20 20:50:24 +0200

    set up local functions' config and initial movieService

--- functions/index.js
+++ functions/index.js
@@ -6,21 +6,27 @@ const { admin, firestore } = require('./config/admin');
 const express = require('express');
 const app = express();
 
+// Services
+const movieService = require('./services/movieService');
+
 // Init
 require('./config/express')(app);
 
 app.get('/', (req, res) => {
-  firestore
-    .collection('users')
-    .get()
-    .then((snapshot) => {
-      snapshot.forEach((doc) => {
-        res.send({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-    });
+  res.send({
+    hello: 'hello',
+  });
+  // firestore
+  //   .collection('users')
+  //   .get()
+  //   .then((snapshot) => {
+  //     snapshot.forEach((doc) => {
+  //       res.send({
+  //         id: doc.id,
+  //         data: doc.data(),
+  //       });
+  //     });
+  //   });
 });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
---
+++ functions/services/movieService.js
@@ -0,0 +1,84 @@
+const functions = require('firebase-functions');
+
+const axios = require('axios').default;
+
+const API_KEY = functions.config().tmdb.key;
+const BASE_URL = functions.config().tmdb.base_url;
+
+const getMovies = async () => {
+  let urlString = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc`;
+
+  let movies = await axios.get(urlString);
+  let moviesArr = extractMovieInfo(movies);
+
+  return moviesArr;
+};
+
+const getPopular = async () => {
+  let urlString = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
+  console.log('urlString', urlString);
+
+  let movies = await axios.get(urlString);
+  let moviesArr = extractMovieInfo(movies);
+
+  return moviesArr;
+};
+
+const getTopRated = async () => {
+  let urlString = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`;
+
+  let movies = await axios.get(urlString);
+  let moviesArr = extractMovieInfo(movies);
+
+  return moviesArr;
+};
+
+const getUpcoming = async () => {
+  let urlString = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US`;
+
+  let movies = await axios.get(urlString);
+  let moviesArr = extractMovieInfo(movies);
+
+  return moviesArr;
+};
+
+function extractMovieInfo(movies) {
+  return movies.data.results.map((movie) => {
+    let imageUrl = 'https://image.tmdb.org/t/p/original' + movie.poster_path;
+    let summary = movie.overview;
+    let date = movie.release_date;
+    let genreIds = movie.genre_ids;
+    let tmdbId = movie.id;
+    let title = movie.title;
+    let popularity = movie.popularity;
+    let voteAverage = movie.vote_average;
+
+    return {
+      imageUrl,
+      summary,
+      date,
+      genreIds,
+      tmdbId,
+      title,
+      popularity,
+      voteAverage,
+    };
+  });
+}
+
+module.exports = {
+  getMovies,
+  getPopular,
+  getTopRated,
+  getUpcoming,
+};
+
+// poster_path
+// overview
+// release_date
+// genre_ids
+// id
+// title
+// popularity
+
+// https://api.themoviedb.org/3/discover/movie?api_key=8ca4deb42599aca624b1c5fa68bcdbfd&language=en-US&sort_by=popularity.desc
